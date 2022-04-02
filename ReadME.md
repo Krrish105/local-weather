79aa1df5fb3ba464d95752512cd683f1f03835e2
Krrish105 <jainkarishma76@gmail.com>
2022-04-02 14:51:38 +0530

    first commit

---
+++ ReadME.md
@@ -0,0 +1,53 @@
+# Local Weather App
+
+This is a solution to the [Show the Local Weather Project](https://www.freecodecamp.org/learn/coding-interview-prep/take-home-projects/show-the-local-weather) from [FreeCodeCamp](https://www.freecodecamp.org/). FreeCodeCamp is a non-profit organization that consists of an interactive learning web platform, an online community forum, chat rooms, online publications and local organizations that intend to make learning web development accessible to anyone. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See the current time
+- See the weather in their current location
+- Toggle the temperature between Fahrenheit and Celsius
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Solution URL: [https://github.com/Krrish105/local-weather](https://github.com/local-weather)
+- Live Site URL: [https://krrish105.github.io/local-weather/](https://krrish105.github.io/local-weather/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [HTML5 Geolocation API](https://www.w3schools.com/html/html5_geolocation.asp)
+- [FreeCodeCamp Weather API](https://weather-proxy.freecodecamp.rocks/)
+
+## Author
+
+- Frontend Mentor - [Krrish105](https://www.frontendmentor.io/profile/Krrish105)
+- Twitter - [_karishma10](https://twitter.com/_karishma10)
+- LinkedIn - [Karishma Garg](https://www.linkedin.com/in/karishma-garg-)
+- Codepen - [Krrish105](https://codepen.io/krrish105)
---
+++ script.js
@@ -0,0 +1,57 @@
+// Variables
+const api = "https://fcc-weather-api.glitch.me/api/current?";
+let unit =0, temp=0, feels=0;
+
+// Sets the Time
+setInterval(() => {
+    let date = new Date();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let timestat = hour <= 12 ? 'a.m.' : 'p.m.';
+    $('#time').text(`${hour}:${min} ${timestat}`)
+}, 10)
+
+// Loads when the document is ready and asks user for their location
+$(document).ready(() => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            getWeather(`lat=${position.coords.latitude}`, `lon=${position.coords.longitude}`);
+        });
+    } else {
+        $('main').hide();
+        $('.error-text').text("Geolocation is not supported by this browser.")
+    }
+})
+
+// Get the weather details and show it to the user
+const getWeather = (lat, lon) => {
+    $.ajax({
+        url: api + lat + '&' + lon,
+        success: (result) => {
+            $("#city").text(`${result.name}, `);
+            $("#country").text(result.sys.country);
+            $("#status").text(result.weather[0].description);
+            temp = Math.round(result.main.temp * 10) / 10;
+            feels = Math.round(result.main.feels_like * 10) / 10;
+            $('#temp').text(temp + " C");
+            $('#feels').text(feels + " C");
+            $('#pressure').text(result.main.pressure);
+            $('#humidity').text(result.main.humidity + '%');
+            $('#weather-icon').attr('src', result.weather[0].icon)
+        }
+    })
+}
+
+
+// Changes the temperature from Celsius to Fahrenheit and vice-versa
+$('#toggle').click(() => {
+    let currentUnit = $('#toggle').text();
+    let newUnit = currentUnit === 'In C' ? 'In F' : 'In C';
+    $('#toggle').text(newUnit);
+    if(newUnit === 'In F'){
+        let newTemp = Math.round(parseInt($("#temp").text()) * 9 / 5 + 32);
+        $("#temp").text(newTemp + ' F');
+    }else{
+        $("#temp").text(temp + ' C');
+    }
+})
